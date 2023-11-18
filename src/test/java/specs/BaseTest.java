214d08cd3f1cd6cccebc9d9f3eb7d65dd1b0dc76
joannak-l <joanna.krahel@gmail.com>
2023-11-18 11:38:55 +0100

    Added functionality to capture screenshots on test failures

--- src/test/java/specs/BaseTest.java
+++ src/test/java/specs/BaseTest.java
@@ -1,9 +1,18 @@
 package specs;
 
 import core.WebDriverFactory;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class BaseTest {
 
@@ -22,9 +31,36 @@ public class BaseTest {
     }
 
     @AfterMethod
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+
+        if (ITestResult.FAILURE == result.getStatus()) {
+            takeScreenshotOnTestFailure(result);
+        }
+
         if (driver != null) {
             driver.quit();
+        }
+    }
+
+    public void takeScreenshotOnTestFailure(ITestResult result) {
+        try {
+            TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+            File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
+
+            String screenshotsDir = "src\\test\\screenshots\\";
+
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyMMdd_HHmm");
+            String timestamp = now.format(formatter);
+
+            String screenshotName = result.getName() + "_" + timestamp + ".png";
+            File destination = new File(screenshotsDir + screenshotName);
+
+            Files.copy(source.toPath(), destination.toPath());
+            System.out.println("Screenshot taken: " + screenshotName);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
